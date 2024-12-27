c080af46c68c61ed54c4b65de35ac0c51412fb45
Anton Sushnikov <resonans93@gmail.com>
2024-12-27 23:15:54 +0500

    feat:stack done

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+    let currentNode = l;
+     while (currentNode) {
+        if (currentNode.value === k) {
+            
+        }
+        currentNode = currentNode.next
+     }
 }
 
 module.exports = {
